f070599d5378642dcd8c074776a0d2d6dd807490
jiaozesheng <jiaozesheng@ipmacro.com>
2015-06-12 15:03:50 +0800

    首页修改1

---
+++ script/my.js
@@ -0,0 +1,53 @@
+function getByClass(selector, parent){
+	parent = parent ? parent : document;
+	var oAll = parent.getElementsByTagName('*');
+	var arr = [];
+	var len = oAll.length;
+	for(var i = 0; i < len; i++){
+		var aAll = oAll[i].className.split(' ');
+		var l = aAll.length;
+		for(var j = 0; j < l; j++){
+			if(aAll[j] == selector){
+				arr.push(oAll[i]);
+				break;
+			}
+		}
+	}
+	return arr;
+}
+
+function getStyle(obj, attr){
+	return obj.currentStyle ? obj.currentStyle(attr) : getComputedStyle(obj, null)[attr];
+}
+
+function startMove(obj, json, fn){
+	clearInterval(obj.timer);
+	obj.timer = setInterval(function(){
+		var bStop = true;		//设置变量判断所有值是否到达目标值
+		for(var attr in json){
+			var iCur = 0;	//获取目标当前值
+			if(attr == 'opacity'){
+				iCur = Math.round(parseFloat(getStyle(obj, attr)) * 100);
+			}else{
+				iCur = parseInt(getStyle(obj, attr));
+			}
+			var iSpeed = (json[attr] - iCur) / 8;	//计算速度
+			iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
+			if(iCur != json[attr]){		//判断是否到达目标值
+				bStop = false;
+			}
+			if(attr == 'opacity'){
+				iCur += iSpeed;
+				obj.style.filter = 'alpha(opacity:'+ iCur +')';
+				obj.style.opacity = iCur / 100;
+			}else{
+				obj.style[attr] = iCur + iSpeed + 'px';
+			}
+		}
+		if(bStop){
+			clearInterval(obj.timer);
+			if(fn)	fn();
+		}
+	}, 30);
+}
+
